dec61d77ad6e463b5735a3c5132e32788094ebaa
Linda <lindaho@hotmail.nl>
2018-01-28 21:23:12 +0100

    28-01-18

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 
-var button = document.querySelector('.like');
+/*like knop*/
+var button = document.querySelector(".like");
+var favoriet = document.querySelector(".favorieten");
 
 var klik = function () {
-    button.classList.toggle('likerood');
+    button.classList.toggle("likerood");
+    favoriet.classList.toggle("favorieten-tekst");
 };
 
-button.addEventListener('click', klik);
+button.addEventListener("click", klik);
+
+/*download knop
+https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_progressbar_3*/
+
+
+function move() {
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+            window.location.assign("favorieten2.html");
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+
+}
